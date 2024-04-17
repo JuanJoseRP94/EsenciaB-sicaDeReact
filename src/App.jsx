15f1da27989aac03f7436d270ddaee1212335ee6
JuanchoRP94 <122208900+JuanJoseRP94@users.noreply.github.com>
2024-04-17 10:59:38 +0000

    Actualización letras aleatorias

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
+import IconImag from "./assets/react-core-concepts.png";
+
+const reactDescriptions = ["fundamentales", "cruciales", "básicos"];
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
 function Header() {
+  const description = reactDescriptions[genRandomInt(2)];
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src={IconImag} alt="Stylized atom" />
       <h1>Esencia de React</h1>
       <p>
-        Conceptos fundamentales de React que necesitaré para la mayoría de
+        Conceptos {description} de React que necesitaré para la mayoría de
         programas que voy a construir.
       </p>
     </header>
